9fc97c5d80328b39a3aaf39997989993ad4a7cf4
paulneo <localos2000@gmail.com>
2017-01-26 21:30:47 -0500

    subiendo mi proyecto  con canvas

---
+++ platzi.js
@@ -0,0 +1,199 @@
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
+
+document.addEventListener("keydown", mover);
+var vp = document.getElementById("villaplatzi");
+var papel = vp.getContext("2d");
+
+var fondo = {
+  url: "tile.png",
+  cargaOK: false
+}
+
+
+var vaca = {
+  url: "vaca.png",
+  cargaOK: false
+};
+var dibujadovaca= true;
+var vacax =[];
+var vacay =[];
+var contadorVaca;
+
+var pollo = {
+  url: "pollo.png",
+  cargaOK: false
+}
+var pollonx =[];
+var pollony =[];
+var dibujadoPollo = true;
+var contadorPollo;
+
+
+var cerdo = {
+  url: "cerdo.png",
+  cargaOK: false,
+  xPosition: 0,
+  yPosition: 0,
+  movimiento: 10
+}
+var dibujadocerdo= true;
+var cantidad = aleatorio(1, 10);
+var puerta = false;
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarpollos);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarcerdos);
+
+function cargarFondo()
+{
+  fondo.cargaOK= true
+  dibujar();
+}
+function cargarVacas()
+{
+  vaca.cargaOK = true;
+  dibujar();
+}
+function cargarpollos()
+{
+
+  pollo.cargaOK = true;
+  dibujar();
+}
+function cargarcerdos(){
+  cerdo.cargaOK=true;
+  dibujar();
+}
+
+function dibujar()
+{
+  if (fondo.cargaOK) {
+
+      papel.drawImage(fondo.imagen, 0, 0);
+      dibujadofondo= false;
+
+  }
+
+  if(vaca.cargaOK)
+  {
+    if (dibujadovaca) {
+      for(var v=0; v < cantidad; v++)
+      {
+        var x = aleatorio(0, 7);
+        var y = aleatorio(0, 10);
+        var x = x * 60;
+        var y = y * 40;
+        papel.drawImage(vaca.imagen, x, y);
+        vacax[v] = x;
+        vacay[v] = y;
+        dibujadovaca =false;
+        contadorVaca = cantidad;
+      }
+
+    }
+  }
+
+  if(pollo.cargaOK)
+  {
+    if (dibujadoPollo) {
+      for(var v=0; v < cantidad; v++)
+      {
+        var x = aleatorio(0, 7);
+        var y = aleatorio(0, 10);
+        var x = x * 60;
+        var y = y * 40;
+        papel.drawImage(pollo.imagen, x, y);
+        pollonx[v]=x;
+        pollony[v]= y;
+        dibujadoPollo=false;
+        contadorPollo = cantidad;
+
+      }
+    }
+
+  }
+
+  if(cerdo.cargaOK)
+  {
+    if (!puerta)
+    {
+      for(var v=0; v < 1; v++)
+        {
+          var xcerdo = aleatorio(0, 7);
+          var ycerdo = aleatorio(0, 10);
+          cerdo.xPosition = xcerdo * 60;
+          cerdo.yPosition = ycerdo * 40;
+          papel.drawImage(cerdo.imagen, cerdo.xPosition, cerdo.yPosition);
+          mover(cerdo.xPosition,cerdo.yPosition);
+
+        }
+    }
+
+    if(puerta){
+      papel.drawImage(cerdo.imagen, cerdo.xPosition, cerdo.yPosition);
+    }
+  }
+  if (!dibujadoPollo && !dibujadovaca) {
+    dibujadovaca=false;
+    if (!dibujadovaca) {
+      for (var i = 0; i < contadorVaca ; i++) {
+        papel.drawImage(vaca.imagen, vacax[i], vacay[i]);
+      }
+    }
+    dibujadoPollo=false;
+    if (!dibujadoPollo) {
+      for (var i = 0; i < contadorPollo; i++) {
+        papel.drawImage(pollo.imagen,pollonx[i], pollony[i] );
+      }
+    }
+  }
+}
+
+
+function mover(evento) {
+    switch(evento.keyCode)
+    {
+      case teclas.UP:
+        cerdo.yPosition = cerdo.yPosition  - cerdo.movimiento;
+        puerta = true;
+        dibujar();
+        break;
+      case teclas.DOWN:
+        cerdo.yPosition  = cerdo.yPosition  + cerdo.movimiento;
+        puerta = true;
+        dibujar();
+        break;
+      case teclas.LEFT:
+        cerdo.xPosition = cerdo.xPosition - cerdo.movimiento;
+        puerta = true;
+        dibujar();
+        break;
+      case teclas.RIGHT:
+        cerdo.xPosition = cerdo.xPosition + cerdo.movimiento;
+        puerta = true;
+        dibujar();
+        break;
+    }
+}
+
+function aleatorio(min, maxi)
+{
+  return Math.floor(Math.random() * (maxi - min + 1)) + min;
+}
